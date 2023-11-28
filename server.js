5798f653c2e5639a881e3a9e4749a174b27407e5
Matt Hall <matt.hall236@icloud.com>
2023-11-27 23:48:11 -0500

    auth0 and git ignore added

---
+++ server.js
@@ -0,0 +1,31 @@
+//SDK Integration
+const { auth } = require('express-openid-connect');
+
+const express = require('express');
+const app = express();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'ey3hd7sV0pLzyq5W28qOHrqTWoVpXnwO',
+  issuerBaseURL: 'https://dev-5zqz1l5bzwzgsdp2.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+//User Profile
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+
