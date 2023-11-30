ce2d78d14e90ad589a9e2357a198c4cb1beb945a
izzybelhall <isabel.w.hall@icloud.com>
2023-11-29 23:00:49 -0500

    days till summer countdown and word count  added

---
+++ days.js
@@ -0,0 +1,31 @@
+let today = new Date();
+let summerYear = today.getFullYear();
+
+if (today.getMonth() > 5 || (today.getMonth() === 5 && today.getDate() > 20)) {
+  summerYear = summerYear + 1;
+}
+
+let summerDate = new Date(summerYear, 5, 20);
+let dayMilliseconds = 1000 * 60 * 60 * 24;
+
+let remainingDays = Math.ceil(
+  (summerDate.getTime() - today.getTime()) /
+   (dayMilliseconds)
+);
+
+let remainingHours = Math.ceil(
+    (summerDate.getTime() - today.getTime()) /
+(1000*60*60)
+);
+
+let remainingMinutes = Math.ceil(
+    (summerDate.getTime() - today.getTime()) /
+(1000*60)
+);
+
+let remainingSeconds = Math.ceil(
+    (summerDate.getTime() - today.getTime()) /
+(1000)
+);
+
+document.write("There are " + remainingDays + " days, " + remainingHours + " hours and " + remainingMinutes + " minutes until Summer Break!");
---
+++ src/word_count.js
@@ -0,0 +1,7 @@
+const textArea= document.getElementById("message");
+const charCount= document.getElementById("cCount");
+
+textArea.oninput= () => {
+    let characters = textArea.value;
+    charCount.textContent = characters.length;
+};
